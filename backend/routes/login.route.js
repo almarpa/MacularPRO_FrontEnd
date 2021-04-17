597155202f554c334fa567876571263432a30c1f
Alejandro <amarcopa@mercadona.es>
2021-04-17 11:25:38 +0200

    fix(backend): remove backen project

--- backend/routes/login.route.js
+++
@@ -1,9 +0,0 @@
-const login = require("../controllers/login.controller");
-var app = require('express').Router();
-
-
-// Login de usuario-medico
-app.post('/', login.autenticate);
- 
-
-module.exports = app; 
